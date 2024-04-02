250b384ab3701d12caad41f6b443446f02661576
Aamir Farooq <aamir@farooq.xyz>
2024-04-02 21:21:24 +0200

    Move stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,45 @@
-import { Input, TextArea } from "@/components/ui/input";
-import SubmitButton from "@/components/ui/submit-button";
 import { auth } from "auth";
-import { submitAppeal } from "./actions/submit-action";
+import Form from "@/components/form";
+import { SessionProvider } from "next-auth/react";
 
 export default async function Page() {
   const session = await auth();
+  const cleanedSession = { expires: "", user: {} };
+
+  if (session?.user) {
+    cleanedSession.user = {
+      name: session.user.name,
+      id: session.user.id,
+    };
+    cleanedSession.expires = session.expires;
+  }
 
   return (
-    <div className="space-y-2">
-      <h1 className="text-3xl font-bold">r/Jailbreak Unban Appeals Form</h1>
-      <p>
-        This form is to be filled out if you were banned from the r/Jailbreak
-        discord server and you are looking to get unbanned. It is not
-        recommended to appeal within 3 months of your ban. Also, you cannot fill
-        this out on behalf of someone else.
-      </p>
-
-      <p>
-        By signing in, you give us access to basic information such as your
-        Discord username and ID. You will also automatically join our appeal
-        server where you will await your decision.
-      </p>
-      {!session || !session.user ? (
+    <SessionProvider session={cleanedSession} basePath="/auth">
+      <div className="space-y-2">
+        <h1 className="text-3xl font-bold">r/Jailbreak Unban Appeals Form</h1>
         <p>
-          To continue, please <em>Sign In</em> with Discord first.
+          This form is to be filled out if you were banned from the r/Jailbreak
+          discord server and you are looking to get unbanned. It is not
+          recommended to appeal within 3 months of your ban. Also, you cannot
+          fill this out on behalf of someone else.
         </p>
-      ) : (
-        <div className="pt-4">
-          <form
-            className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-            action={submitAppeal}
-          >
-            <label className="font-medium text-sm" htmlFor="username">
-              Username
-            </label>
-            <Input
-              name="username"
-              type="text"
-              placeholder="Discord Username"
-              disabled
-              value={session.user.name!}
-            />
-            <label className="font-medium text-sm" htmlFor="id">
-              ID
-            </label>
-            <Input
-              name="id"
-              type="text"
-              placeholder="Discord ID"
-              disabled
-              value={session.user.id!}
-            />
 
-            <label className="font-medium text-sm" htmlFor="whenBanned">
-              Approximately when were you banned?
-            </label>
-            <Input name="whenBanned" type="date" placeholder="Discord ID" />
-            <label className="font-medium text-sm" htmlFor="whyBanned">
-              Why were you banned?
-            </label>
-            <TextArea name="whyBanned" rows={4} />
-            <label className="font-medium text-sm" htmlFor="banJustified">
-              Do you think the ban was justified? Explain your answer.
-            </label>
-            <TextArea name="banJustified" rows={4} />
-            <label className="font-medium text-sm" htmlFor="whyUnban">
-              Why do you think you should be unbanned?
-            </label>
-            <TextArea name="whyUnban" rows={4} />
-            <SubmitButton />
-          </form>
-        </div>
-      )}
-    </div>
+        <p>
+          By signing in, you give us access to basic information such as your
+          Discord username and ID. You will also automatically join our appeal
+          server where you will await your decision.
+        </p>
+        {!session || !session.user ? (
+          <p>
+            To continue, please <em>Sign In</em> with Discord first.
+          </p>
+        ) : (
+          <div className="pt-4">
+            <Form />
+          </div>
+        )}
+      </div>
+    </SessionProvider>
   );
 }
---
+++ components/form.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { submitAppeal } from "@/actions/submit-action";
+import { Input, TextArea } from "@/components/ui/input";
+import SubmitButton from "@/components/ui/submit-button";
+import { useSession } from "next-auth/react";
+import React from "react";
+
+const Form = () => {
+  const { data: session } = useSession();
+  if (!session?.user) {
+    return (
+      <p>
+        To continue, please <em>Sign In</em> with Discord first.
+      </p>
+    );
+  }
+
+  return (
+    <form
+      className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+      action={submitAppeal}
+    >
+      <label className="font-medium text-sm" htmlFor="username">
+        Username
+      </label>
+      <Input
+        name="username"
+        type="text"
+        placeholder="Discord Username"
+        disabled
+        value={session.user.name!}
+      />
+      <label className="font-medium text-sm" htmlFor="id">
+        ID
+      </label>
+      <Input
+        name="id"
+        type="text"
+        placeholder="Discord ID"
+        disabled
+        value={session.user.id!}
+      />
+
+      <label className="font-medium text-sm" htmlFor="whenBanned">
+        Approximately when were you banned?
+      </label>
+      <Input name="whenBanned" type="date" placeholder="Discord ID" />
+      <label className="font-medium text-sm" htmlFor="whyBanned">
+        Why were you banned?
+      </label>
+      <TextArea name="whyBanned" rows={4} />
+      <label className="font-medium text-sm" htmlFor="banJustified">
+        Do you think the ban was justified? Explain your answer.
+      </label>
+      <TextArea name="banJustified" rows={4} />
+      <label className="font-medium text-sm" htmlFor="whyUnban">
+        Why do you think you should be unbanned?
+      </label>
+      <TextArea name="whyUnban" rows={4} />
+      <SubmitButton />
+    </form>
+  );
+};
+
+export default Form;
