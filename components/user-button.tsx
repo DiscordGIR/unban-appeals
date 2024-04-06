3053857776c67f1838ce4adbff68da5c22d30043
Aamir Farooq <aamir@farooq.xyz>
2024-04-07 00:24:38 +0200

    Show user ID in navbar dropdown

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -39,7 +39,7 @@ export default async function UserButton() {
                 {session.user.name}
               </p>
               <p className="text-xs leading-none text-muted-foreground">
-                {session.user.email}
+                {session.user.id}
               </p>
             </div>
           </DropdownMenuLabel>
